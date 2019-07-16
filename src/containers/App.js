81067843ea17e03cd0e4c9fbddfdad126552dde9
Michael Williams <bsdfool@gmail.com>
2019-07-15 21:00:19 -0400

    add redux-thunk, move state into redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,36 +6,37 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        isPending: state.requestRobots.isPending,
+        robots: state.requestRobots.robots,
+        error: state.requestRobots.error
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: []
-        }
+
+    componentDidMount() {
+        this.props.onRequestRobots();
     }
 
     render() {
-        const { robots } = this.state;
-        const { searchField, onSearchChange } = this.props;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
-        return !robots.length ?
-            <h1>Loading...</h1> :
+        return isPending ?
+            <h1>Loading</h1> :
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
@@ -47,13 +48,6 @@ class App extends Component {
                     </Scroll>
                 </div>
             );
-    }
-
-    componentDidMount() {
-        // console.log(this.props.store.getState());
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((res) => res.json())
-            .then((users) => this.setState({ robots: users }));
     }
 }
 
